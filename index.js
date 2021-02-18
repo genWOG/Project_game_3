6ed3418377a0f7c03b6364ea0eea5c3206d60d52
genWOG <gen.smirnov.dn@gmail.com>
2021-02-18 15:21:13 +0300

    New clicks

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var $start = document.querySelector('#start');
 var $game = document.querySelector('#game');
 var square; //for quares generation 
-var score;
+var score = 0;
+var square_size;
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', square_click);
@@ -17,24 +18,26 @@ function square_click(event) {
     $game.innerHTML = ''; //disallow the game to duplicate square if we already have a square
     if (event.target.dataset) {
         score++;
+        console.log(score);
         square_generator();
     }
 }
 
 //generate random wquares in the gameField
 function square_generator() {
+    square_size = getRand(20, 100);
     square = document.createElement('div');
-    square.style.height = square.style.width = '50px';
+    square.style.height = square.style.width = square_size + 'px';
     square.style.position = 'absolute'; //temporary
     square.style.backgroundColor = 'Pink';
     square.style.cursor = 'pointer';
     square.style.top = '50px';
     square.style.left = '50px';
-    square.setAttribute('data-square', 'true'); //"data-" then "some text"
+    square.setAttribute('data-square', 'true'); //"data-" then "some text"?  ??
     //after install all the properties, create Element in the field
     $game.insertAdjacentElement('afterbegin', square);
 }
 
-function getRand() {
-
+function getRand(min, max) {
+    return Math.floor(Math.random() * (max, min) + min); //1.9130130 -> 1
 }
